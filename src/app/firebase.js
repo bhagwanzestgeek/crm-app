77d72885643400bd8da257649b087d4fcfdf0a39
= <guptavaishnavi671@gmail.com>
2022-02-08 10:56:17 +0530

    setup firevase

---
+++ src/app/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDckP_RtoB9qhcENAAdmdILSVRe34btOH8",
+  authDomain: "crmz-e12b4.firebaseapp.com",
+  projectId: "crmz-e12b4",
+  storageBucket: "crmz-e12b4.appspot.com",
+  messagingSenderId: "1060737088640",
+  appId: "1:1060737088640:web:4459dca5be70d33480fa65",
+  measurementId: "G-RD6Z35KTVB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
